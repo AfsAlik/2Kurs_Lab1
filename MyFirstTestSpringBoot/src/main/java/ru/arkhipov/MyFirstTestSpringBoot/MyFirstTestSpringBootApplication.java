165d1fbdddd75c316e5e93bb8f30668bee723287
AfsAlik <102171214+AfsAlik@users.noreply.github.com>
2024-09-28 18:30:15 +0500

    Lab 1-2
    Working on Lab 1-2

---
+++ MyFirstTestSpringBoot/src/main/java/ru/arkhipov/MyFirstTestSpringBoot/MyFirstTestSpringBootApplication.java
@@ -0,0 +1,13 @@
+package ru.arkhipov.MyFirstTestSpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyFirstTestSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyFirstTestSpringBootApplication.class, args);
+	}
+
+}
---
+++ MyFirstTestSpringBoot/src/test/java/ru/arkhipov/MyFirstTestSpringBoot/MyFirstTestSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.arkhipov.MyFirstTestSpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyFirstTestSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
